19ff00f01137728a4a75ec584c98f2c0018bed15
wiresnchains <dgrachov@icloud.com>
2025-12-16 09:46:28 +0100

    Added cors configuration

--- src/main/java/org/example/storymasters/Router.java
+++ src/main/java/org/example/storymasters/Router.java
@@ -7,7 +7,15 @@ public class Router {
     private final Javalin app;
 
     public Router() {
-        app = Javalin.create();
+        app = Javalin.create(config -> {
+            config.bundledPlugins.enableCors(cors -> {
+                cors.addRule(rule -> {
+                    rule.allowHost("http://localhost:3000");
+                    rule.allowHost("https://wiresnchains.com");
+                    rule.allowCredentials = true;
+                });
+            });
+        });
     }
 
     public void run(int port) {
